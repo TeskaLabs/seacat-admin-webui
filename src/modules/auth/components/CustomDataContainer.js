644b8b58fe1c02aac3d6261e6d5feb872a48041c
Petr Kavulok <petrkavulok@Petrs-MacBook-Pro.local>
2023-01-05 09:53:16 +0100

    docs  adjustment

--- src/modules/auth/components/CustomDataContainer.js
+++ src/modules/auth/components/CustomDataContainer.js
@@ -14,11 +14,13 @@ import {
 /*
 	Renders plug'n'play custom data card.
 
-	resources: array, of credential's resources - used for ButtonWithAuthz granting option to edit Custom data
-	customData: object, containing data provided by api
-	loading: boolean, comming from the parent component deciding whether the data has already been fetched or not. if false, displays content loader
-	resource: string, resource granting option to edit CustomData
-	uri: string, SeaCatAuths API endpoint do save edited data. should be in format 'credentials/mongodb:JJ68' for unification, avoid passing '/credentials/mongodb:JJ68' with slash as the first character
+	- app
+	- resources: array, of credential's resources - used for ButtonWithAuthz granting option to edit Custom data
+	- customData: object, containing data provided by api
+	- setCustomData: function managing customData's state in parent component
+	- loading: boolean, comming from the parent component deciding whether the data has already been fetched or not. if false, displays content loader
+	- resource: string, resource granting option to edit CustomData
+	- uri: string, SeaCatAuths API endpoint do save edited data. should be in format 'credentials/mongodb:JJ68' for unification, avoid passing '/credentials/mongodb:JJ68' with slash as the first character
 */
 
 export function CustomDataContainer({app, resources, customData, setCustomData, loading, resource = "authz:superuser", uri}) {
