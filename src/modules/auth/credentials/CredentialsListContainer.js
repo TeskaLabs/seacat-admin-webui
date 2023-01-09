a6fd401e2f1b21d0eda9db47572d7a04890c7610
Pe5h4 <pesekf@email.cz>
2023-01-09 13:32:46 +0100

    Update locales for credentialslist container

--- src/modules/auth/credentials/CredentialsListContainer.js
+++ src/modules/auth/credentials/CredentialsListContainer.js
@@ -160,10 +160,10 @@ function CredentialsListContainer(props) {
 			console.error(e);
 			setLoading(false);
 			if (e.response.status === 401) {
-				props.app.addAlert("warning", t("CredentialsListContainer|Can't fetch the data, you don't have rights to display it"));
+				props.app.addAlert("warning", t("CredentialsListContainer|Can't fetch the data, you don't have rights to display it"), 30);
 				return;
 			}
-			props.app.addAlert("warning", t("CredentialsListContainer|Something went wrong, failed to fetch data"));
+			props.app.addAlert("warning", t("CredentialsListContainer|Something went wrong, failed to fetch data", {error: e?.response?.data?.message}), 30);
 		}
 	};
 
@@ -179,7 +179,7 @@ function CredentialsListContainer(props) {
 
 		} catch (e) {
 			console.error(e);
-			props.app.addAlert("warning", t("CredentialsListContainer|Something went wrong, failed to fetch data"));
+			props.app.addAlert("warning", t("CredentialsListContainer|Something went wrong, failed to fetch data", {error: e?.response?.data?.message}), 30);
 		}
 	}
 
@@ -195,7 +195,7 @@ function CredentialsListContainer(props) {
 
 		} catch (e) {
 			console.error(e);
-			props.app.addAlert("warning", t("CredentialsListContainer|Something went wrong, failed to fetch data"));
+			props.app.addAlert("warning", t("CredentialsListContainer|Something went wrong, failed to fetch data", {error: e?.response?.data?.message}), 30);
 		}
 	}
 
