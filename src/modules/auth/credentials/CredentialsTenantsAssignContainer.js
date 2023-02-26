f5de62bf383bc47f79b31509efc0a45ea0de28f9
petrKavulok <petr.kavulok@gmail.com>
2023-02-26 21:36:24 +0800

    Tenants card init. fetchAllTenants function and display in form with checkboxes

--- src/modules/auth/credentials/CredentialsTenantsAssignContainer.js
+++ src/modules/auth/credentials/CredentialsTenantsAssignContainer.js
@@ -1,8 +1,84 @@
-import React from 'react'
+import React, { useState, useEffect } from "react"
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { useSelector } from "react-redux";
 
 const CredentialsTenantsAssignContainer = (props) => {
-    return(
-        <p>hello</p>
+
+	const {t} = useTranslation();
+    const [ allTenants, setAllTenants] = useState(undefined);
+
+	const { register, handleSubmit, reset } = useForm({defaultValues: { tenants: assignedTenants }});
+    const resources = useSelector(state => state.auth?.resources);
+
+    useEffect(() => {
+        fetchAllTenants();
+    })
+
+    const fetchAllTenants = async() => {
+		try {
+			let eligibleTenants = [];
+			let response = await SeaCatAuthAPI.get('/tenants', {params: {f: filter, i: limit}});
+			// if a logged in user doesn't hold 'authz:superuser' resource, their rights allow them to only assing the tenant, which they are currently using
+			if (resources.includes('authz:superuser')) {
+				eligibleTenants = response.data.data;
+			} else if (response.data.data.length > 0) {
+				let filteredTenant = response.data.data.filter(obj => obj._id === `${tenant}`);
+				eligibleTenants.push(filteredTenant[0]);
+			}
+			// this function changed eligibleTenants order to alphabetical
+			eligibleTenants.sort((a, b) => a._id.localeCompare(b._id));
+			setAllTenants(eligibleTenants)
+			setCount(response.data.count);
+		} catch(e) {
+			console.error(e);
+			// props.app.addAlert("warning", `${t("CredentialsTenantsCard|Something went wrong, failed to fetch tenants")}. ${e?.response?.data?.message}`, 30);
+		}
+	};
+
+    const submit = (data) => {
+        // TBD
+    }
+
+    return (
+        <Container>
+			<Form onSubmit={handleSubmit(submit)} className="assign-tenants-wraper">
+
+                <Card className="assign-tenants-tenants w-30">
+                    <CardHeader>
+                        <div className="card-header-title">
+                            <i className="cil-apps mr-2" />
+                            {t("CredentialsTenantsAssignContainer|Assign tenants")}
+                        </div>
+                    </CardHeader>
+
+                    <CardBody>
+                        <Col>
+                        {allTenants && allTenants.length > 0 ?
+                        allTenants.map((tenant) => {
+                            return(
+                                <Row key={tenant._id}>
+                                    <input
+                                        type="checkbox"
+                                        value={tenant._id}
+                                        {...register("tenants")}
+                                    />
+                                    {tenant._id}
+                                </Row>
+                            )
+                        })
+                        : <p>{t("CredentialsTenantsAssignContainer|No data")}</p>}
+                        </Col>
+                    </CardBody>
+
+                    <CardFooter className="border-top">
+                        <ButtonGroup className="flex-nowrap">
+                            <Button color="primary" type="submit"> {t("Save")} </Button>
+                        </ButtonGroup>
+                    </CardFooter>
+                </Card>
+			</Form>
+		</Container>
     )
 }
 
