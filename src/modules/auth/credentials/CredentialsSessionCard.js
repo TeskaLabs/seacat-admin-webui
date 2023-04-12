43586664639de384a3055207a1036ddc5f4a19b0
Pe5h4 <pesekf@email.cz>
2023-04-12 10:17:52 +0200

    Update resource

--- src/modules/auth/credentials/CredentialsSessionCard.js
+++ src/modules/auth/credentials/CredentialsSessionCard.js
@@ -66,7 +66,7 @@ function CredentialsSessionCard(props) {
 					color="danger"
 					outline
 					onClick={() => {terminateSessionsForm()}}
-					resource="authz:superuser" // Only superusers can terminate all sessions
+					resource="seacat:session:terminate"
 					resources={props.resources}
 					disabled={data.length == 0}
 				>
