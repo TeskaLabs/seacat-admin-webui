32b65f7da13c288486e1d00a4f4d0e257ed7c6b1
petrKavulok <petr.kavulok@gmail.com>
2023-04-19 17:11:15 +0200

    refactored roles sorting in role dropdown, translations update

--- src/modules/auth/components/RoleDropdown.js
+++ src/modules/auth/components/RoleDropdown.js
@@ -1,63 +1,48 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Dropdown, DropdownToggle,
 	DropdownItem, DropdownMenu, Input } from "reactstrap";
 
-export default function RoleDropdown({props, tenantObj, selectedTenants, setSelectedTenants, idx}) {
+const RoleDropdown = React.memo(({props, tenantObj, selectedTenants, setSelectedTenants, idx}) => {
 
 	const { t } = useTranslation();
 	const [dropdownOpen, setDropdownOpen] = useState(false);
-	const [displayRoles, setDisplayRoles] = useState({data: []});
+	const [displayTenantRoles, setDisplayTenantRoles] = useState({data: []});
 	const [count, setCount] = useState(0);
 	const [filter, setFilter] = useState("");
 	const [limit, setLimit] = useState(5);
+	const [items, setItems] = useState(0)
 
 	let SeaCatAuthAPI = props.app.axiosCreate('seacat_auth');
 
-	const timeoutRef = useRef(null);
-	// useEffect(() => {
-	// 	if(tenantObj?.roles) {
-	// 		let obj = {...tenantObj.roles};
-	// 		setDisplayRoles(obj);
-	// 	}
-	// }, []);
-
-	// useEffect(() => {
-	// 	console.log('hello form ln25')
-	// 	retrieveRoleList(tenantObj)
-	// }, []);
 	useEffect(() => {
-		console.log('hello form ln25')
-		retrieveRoleList(tenantObj)
+		retrieveRoleList(tenantObj);
 	}, [limit]);
 
-	// TODO: this useeffect should match selected roles with the ones we want to display and readd the ones which were removed from selected roles in BulkAssignmentContainer
-	// useEffect(() => {
-	// 	if (tenantObj.selectedRole && (tenantObj.selectedRole.length > 0)) {
-	// 		let display = {...displayRoles};
-	// 		let newDisplayData = [];
-	// 		selectedTenants[idx].roles.data.map((tenantRole) => {
-	// 			let found = selectedTenants[idx].selectedRole.find(el => el === tenantRole._id);
-	// 			if (!found) {
-	// 				newDisplayData.push(tenantRole);
-	// 			}
-	// 		});
-	// 		display.data = newDisplayData;
-	// 		setDisplayRoles(display);
-	// 	}
-	// 	if (tenantObj.selectedRole && tenantObj.selectedRole.length === 0) {
-	// 		setDisplayRoles(selectedTenants[idx].roles);
-	// 	}
-	// }, [selectedTenants]);
-
-
-	useEffect(() => {
-		let obj = {...tenantObj?.roles};
-		setDisplayRoles(obj);
-	}, []);
+	// TODO: this useEffect should match selected roles with the ones we want to display and re-add the ones which were removed from selected roles in BulkAssignmentContainer
+	useEffect(() => {
+		if (tenantObj.selectedRole && (tenantObj.selectedRole.length > 0)) {
+			let display = {...displayTenantRoles};
+			// newDisplayData will hold roles coming from the service minus the ones we've already assigned
+			let newDisplayData = [];
+			// selectedTenants[idx]?.roles?.data.map((tenantRole) => {
+			tenantObj?.roles?.data.map((tenantRole) => {
+				// we are comparing already selected roles with all roles and keeping only the elements, which are not included in selectedRole(s)
+				let found = tenantObj?.selectedRole.find(el => el === tenantRole._id);
+				if (!found) {
+					newDisplayData.push(tenantRole);
+				}
+			});
+			display['data'] = newDisplayData;
+			setDisplayTenantRoles(display);
+		}
+		if (tenantObj.selectedRole && tenantObj.selectedRole.length === 0) {
+			setDisplayTenantRoles(tenantObj.roles);
+		}
+	}, [selectedTenants, items]);
 
 // uncomment, when search functionality in roles is enabled on the backend
-	//sets 0.5s delay before triggering the search call when filtering through tennants
+	// // sets 0.5s delay before triggering the search call when filtering through tennants
 	// useEffect(() => {
 	// 	if (timeoutRef.current !== null) {
 	// 		clearTimeout(timeoutRef.current);
@@ -68,66 +53,73 @@ export default function RoleDropdown({props, tenantObj, selectedTenants, setSele
 	// 	}, 500);
 	// }, [filter]);
 
+	// const handleFilter = (e) => {
+	// 	setLimit(5);
+	// 	setFilter(e.target.value);
+	// }
 
 	// fetch roles for Tenant dropdowns
 	const retrieveRoleList = async (tenantObj) => {
 		let response;
-		let arr;
+		let id = tenantObj._id;
+		let parameters = {f: filter, i: limit, exclude_global: true};
+		if (tenantObj._id === "Global roles") {
+			id = '*';
+			parameters['exclude_global'] = false;
+		}
 		try {
-			response = await SeaCatAuthAPI.get(`/role/${tenantObj._id}`, {params: {f: filter, i: limit}});
+			response = await SeaCatAuthAPI.get(`/role/${id}`, {params: parameters});
 			tenantObj['roles'] = response.data;
-			// arr = [...selectedTenants, tenantObj];
-			// setSelectedTenants(arr);
-			console.log('response data from API call: ', response.data.data);
 			setCount(response.data.count);
-			setDisplayRoles(response.data);
+			/* this Items state was created just as a helper to cause triggering of useEffect
+			on line 23 when new roles were triggered after pressing the more button in role dropdown.
+			This is not the most elegant solution, I'm open for updgrade */
+			setItems(response.data.data.length);
+			setDisplayTenantRoles(response.data);
 		} catch (e) {
 			console.error(e);
 			props.app.addAlert("warning", `${t("BulkAssignmentContainer|Failed to fetch roles")}. ${e?.response?.data?.message}`, 30);
 		}
 	}
 
+	// adds role to the list of selected roles
 	const addRole = (roleId, index) => {
-		console.log('inside addRole Function: roleId: ', roleId, 'index: ', index);
 		let tenants = [...selectedTenants];
 		let tenant = {...tenantObj};
-		let displayTenantRoles = [...displayRoles.data];
-		displayTenantRoles.splice(index, 1);
-		setDisplayRoles({...displayRoles, data: displayTenantRoles});
+		//create a copy of displayed roles
+		let displayTenantRolesCopy = [...displayTenantRoles?.data];
+		// remove selected item from the copy of displayed roles
+		displayTenantRolesCopy.splice(index, 1);
+		//update state with modified copy
+		setDisplayTenantRoles({...displayTenantRoles, data: displayTenantRolesCopy});
 		if (tenant.selectedRole) {
-			Object.assign(tenant, {'selectedRole': [...tenant?.selectedRole, roleId] });
+			Object.assign(tenant, {'selectedRole': [...tenant.selectedRole, roleId] });
 		} else {
-			Object.assign(tenant, {'selectedRole': [ roleId] });
+			Object.assign(tenant, {'selectedRole': [roleId] });
 		};
 		tenants[idx] = tenant;
 		setSelectedTenants(tenants);
 	};
 
-	const handleFilter = (e) => {
-		setLimit(5);
-		setFilter(e.target.value);
-	}
-
 	return (
 		<Dropdown className='ml-auto' size="sm" isOpen={dropdownOpen} toggle={() => setDropdownOpen(prev => !prev)}>
 			<DropdownToggle caret outline color="primary">
 				<span className="cil-plus mr-2" />
-				{t("RolesResourcesCard|Add role")}
+				{t("BulkAssignmentContainer|Add role")}
 			</DropdownToggle>
 			<DropdownMenu  style={{maxHeight: "20em", overflowY: "auto"}} >
-				<DropdownItem header>{t("RolesResourcesCard|Select role")}</DropdownItem>
+				<DropdownItem header>{t("BulkAssignmentContainer|Select roles")}</DropdownItem>
 
-{/* uncomment, when search functionality in roles is enabled on the backend
+			{/* uncomment, when search functionality in roles is enabled on the backend
 				<Input
-					placeholder={t("x|Search")}
+					placeholder={t("BulkAssignmentContainer|Search")}
 					className="m-0"
 					onChange={e => handleFilter(e)}
 					value={filter}
 				/> */}
-				{displayRoles?.data && displayRoles.data.map((role, i) => (
+				{displayTenantRoles?.data && displayTenantRoles.data.map((role, i) => (
 					<DropdownItem key={idx} onClick={() => addRole(role._id, i)}>
 						{role._id}
-
 					</DropdownItem>
 				))}
 				{(count > limit) ?
@@ -135,11 +127,11 @@ export default function RoleDropdown({props, tenantObj, selectedTenants, setSele
 						<DropdownItem divider />
 						<DropdownItem
 							onClick={() => {
-								setLimit(limit + 5);
+								setLimit(prev => prev + 5);
 								setDropdownOpen(prev => !prev);
 							}}
 						>
-							{t("X|More")}
+							{t("BulkAssignmentContainer|More")}
 						</DropdownItem>
 					</>
 					:
@@ -148,4 +140,6 @@ export default function RoleDropdown({props, tenantObj, selectedTenants, setSele
 			</DropdownMenu>
 		</Dropdown>
 	)
-}
+});
+
+export default RoleDropdown
