5ec289b0aea0f451918fcbbdfddcc3235f2def3b
petrKavulok <petr.kavulok@gmail.com>
2023-04-20 11:23:34 +0200

    tooltip init

--- src/modules/auth/components/RoleDropdown.js
+++ src/modules/auth/components/RoleDropdown.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Dropdown, DropdownToggle,
-	DropdownItem, DropdownMenu, Input } from "reactstrap";
+	DropdownItem, DropdownMenu, Input,
+	UncontrolledTooltip } from "reactstrap";
 
 const RoleDropdown = React.memo(({props, tenantObj, selectedTenants, setSelectedTenants, idx}) => {
 
@@ -11,9 +12,10 @@ const RoleDropdown = React.memo(({props, tenantObj, selectedTenants, setSelected
 	const [count, setCount] = useState(0);
 	const [filter, setFilter] = useState("");
 	const [limit, setLimit] = useState(5);
-	const [items, setItems] = useState(0)
+	const [items, setItems] = useState(0);
 
 	let SeaCatAuthAPI = props.app.axiosCreate('seacat_auth');
+	let message;
 
 	useEffect(() => {
 		retrieveRoleList(tenantObj);
@@ -21,7 +23,7 @@ const RoleDropdown = React.memo(({props, tenantObj, selectedTenants, setSelected
 
 	// TODO: this useEffect should match selected roles with the ones we want to display and re-add the ones which were removed from selected roles in BulkAssignmentContainer
 	useEffect(() => {
-		if (tenantObj.selectedRole && (tenantObj.selectedRole.length > 0)) {
+		if (tenantObj.selectedRole && (tenantObj?.selectedRole?.length > 0)) {
 			let display = {...displayTenantRoles};
 			// newDisplayData will hold roles coming from the service minus the ones we've already assigned
 			let newDisplayData = [];
@@ -36,7 +38,7 @@ const RoleDropdown = React.memo(({props, tenantObj, selectedTenants, setSelected
 			display['data'] = newDisplayData;
 			setDisplayTenantRoles(display);
 		}
-		if (tenantObj.selectedRole && tenantObj.selectedRole.length === 0) {
+		if (tenantObj.selectedRole && tenantObj?.selectedRole?.length === 0) {
 			setDisplayTenantRoles(tenantObj.roles);
 		}
 	}, [selectedTenants, items]);
@@ -74,7 +76,7 @@ const RoleDropdown = React.memo(({props, tenantObj, selectedTenants, setSelected
 			/* this Items state was created just as a helper to cause triggering of useEffect
 			on line 23 when new roles were triggered after pressing the more button in role dropdown.
 			This is not the most elegant solution, I'm open for updgrade */
-			setItems(response.data.data.length);
+			setItems(response?.data?.data.length);
 			setDisplayTenantRoles(response.data);
 		} catch (e) {
 			console.error(e);
@@ -101,44 +103,61 @@ const RoleDropdown = React.memo(({props, tenantObj, selectedTenants, setSelected
 		setSelectedTenants(tenants);
 	};
 
-	return (
-		<Dropdown className='ml-auto' size="sm" isOpen={dropdownOpen} toggle={() => setDropdownOpen(prev => !prev)}>
-			<DropdownToggle caret outline color="primary">
-				<span className="cil-plus mr-2" />
-				{t("BulkAssignmentContainer|Add role")}
-			</DropdownToggle>
-			<DropdownMenu  style={{maxHeight: "20em", overflowY: "auto"}} >
-				<DropdownItem header>{t("BulkAssignmentContainer|Select roles")}</DropdownItem>
+	// determins tooltip messages
+	if((tenantObj._id === "Global roles")) {
+		(tenantObj?.selectedRole && (tenantObj.selectedRole.length > 0)) ?
+			message = t("BulkAssignmentContainer|Selected global roles will be assignes to/unassigned from the selected credentials")
+			: message = t("BulkAssignmentContainer|Global roles will not be manipulated in any way")
+	} else if (tenantObj?.selectedRole && (tenantObj.selectedRole.length > 0)) {
+		message = t("BulkAssignmentContainer|Tenant ") + tenantObj._id + t("BulkAssignmentContainer|and selected roles will be assigned to/unassigned from selected credentials");
+	} else {
+		message = t("BulkAssignmentContainer|Selected credentials will be assigned to/removed from tenant ") + tenantObj._id
+	}
 
-			{/* uncomment, when search functionality in roles is enabled on the backend
-				<Input
-					placeholder={t("BulkAssignmentContainer|Search")}
-					className="m-0"
-					onChange={e => handleFilter(e)}
-					value={filter}
-				/> */}
-				{displayTenantRoles?.data && displayTenantRoles.data.map((role, i) => (
-					<DropdownItem key={idx} onClick={() => addRole(role._id, i)}>
-						{role._id}
-					</DropdownItem>
-				))}
-				{(count > limit) ?
-					<>
-						<DropdownItem divider />
-						<DropdownItem
-							onClick={() => {
-								setLimit(prev => prev + 5);
-								setDropdownOpen(prev => !prev);
-							}}
-						>
-							{t("BulkAssignmentContainer|More")}
+	return (
+		<>
+			<span href="#" id={`tooltip-${idx}`}> <i className='cil-info ml-2'></i></span>
+			<UncontrolledTooltip placement="right" target={`tooltip-${idx}`}>
+				{message}
+			</UncontrolledTooltip>
+			<Dropdown className='ml-auto' size="sm" isOpen={dropdownOpen} toggle={() => setDropdownOpen(prev => !prev)}>
+				<DropdownToggle caret outline color="primary">
+					<span className="cil-plus mr-2" />
+					{t("BulkAssignmentContainer|Add role")}
+				</DropdownToggle>
+				<DropdownMenu  style={{maxHeight: "20em", overflowY: "auto"}} >
+					<DropdownItem header>{t("BulkAssignmentContainer|Select roles")}</DropdownItem>
+
+				{/* uncomment, when search functionality in roles is enabled on the backend
+					<Input
+						placeholder={t("BulkAssignmentContainer|Search")}
+						className="m-0"
+						onChange={e => handleFilter(e)}
+						value={filter}
+					/> */}
+					{displayTenantRoles?.data && displayTenantRoles.data.map((role, i) => (
+						<DropdownItem key={idx} onClick={() => addRole(role._id, i)}>
+							{role._id}
 						</DropdownItem>
-					</>
-					:
-					null
-				}
-			</DropdownMenu>
-		</Dropdown>
+					))}
+					{(count > limit) ?
+						<>
+							<DropdownItem divider />
+							<DropdownItem
+								onClick={() => {
+									setLimit(prev => prev + 5);
+									setDropdownOpen(prev => !prev);
+								}}
+								>
+								{t("BulkAssignmentContainer|More")}
+							</DropdownItem>
+						</>
+						:
+						null
+					}
+				</DropdownMenu>
+			</Dropdown>
+		</>
 	)
 });
 
