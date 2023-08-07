16b535c82578e5441a73f27ba25d94c587acf466
akonshina <arina.konshina@teskalabs.com>
2023-08-07 06:45:09 +0200

    rename user to credentials in the alert

--- src/modules/auth/credentials/CredentialsDetailContainer.js
+++ src/modules/auth/credentials/CredentialsDetailContainer.js
@@ -94,7 +94,7 @@ function CredentialsDetailContainer(props) {
 				setEditable(false);
 			}
 			if (response.data.editable !== true) {
-				props.app.addAlert("warning", `${t("CredentialsDetailContainer|Editing is disabled for this user")}.`, 30);
+				props.app.addAlert("warning", `${t("CredentialsDetailContainer|Editing is disabled for this credential")}.`, 30);
 			}
 		} catch(e) {
 			console.error(e);
